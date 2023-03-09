81748430e5b81c422b435259fd2e582f91519094
edgarrps <edgarrps@gmail.com>
2023-03-09 17:49:48 -0400

    Initial format of project

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -1,5 +1,15 @@
-export default function Finance (){
-return(
-    <div className='p-10 font-bold text-2xl text-center'>Finance App</div>
-)
+export default function Finance() {
+    return (
+        <>
+            <h1 className='p-5 pb-12 bg-green-400 text-stone-50 font-bold text-2xl text-center'>Finance App</h1>
+            <div className='m-auto -mt-9 max-w-[980px] min-h-[600px] mb-[40px] rounded-t-sm shadow-xl text-center bg-stone-100'>
+                <p className='pt-5 font-semibold'>H2</p>
+
+                <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1'>
+                    <p className='pt-5 font-semibold'>Grid col 1</p>
+                    <p className='pt-5 font-semibold'>Grid col 2</p>
+                </div>
+            </div>
+        </>
+    )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body:['Manrope']
+      }
+    },
   },
   plugins: [],
 }
