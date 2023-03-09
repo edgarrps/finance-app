f4e48f120acd4de39a83e2b4cd792b523bb297b9
edgarrps <edgarrps@gmail.com>
2023-03-09 14:58:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Finance from './pages/Finance'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Finance />
+  </React.StrictMode>,
+)
+
---
+++ src/pages/Finance.tsx
@@ -0,0 +1,5 @@
+export default function Finance (){
+return(
+    <div className='p-10 font-bold text-2xl text-center'>Finance App</div>
+)
+}
