587c6251e55e6b91ab95685ca3d2bffbc247d7bb
Edgar Santos <edgarrps@gmail.com>
2023-03-13 16:06:34 -0400

    Fixed tags

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -1,18 +1,18 @@
 import { Item } from "../types/Item"
 import { formatDate } from "../helpers/dateFilter"
-
+import { categories } from "../data/categories"
 type Props = {
     item: Item
 }
 
-export default function TableItem({item}: Props) {
+export default function TableItem({ item }: Props) {
 
     return (
         <tr className='h-10'>
             <td>{formatDate(item.date)}</td>
-            <td>{item.category}</td>
+            <td>{categories[item.category].title}</td>
             <td>{item.description}</td>
-            <td>{item.value}</td>
+            <td>$ {item.value}</td>
         </tr>
     )
 }
