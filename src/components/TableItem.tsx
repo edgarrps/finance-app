eb6c575a3098437d9f8cee2e4e5b3d9f6ca946aa
Edgar Santos <edgarrps@gmail.com>
2023-03-13 20:04:24 -0400

    style of table value

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -8,11 +8,11 @@ type Props = {
 export default function TableItem({ item }: Props) {
 
     return (
-        <tr className='h-10 border'>
+        <tr className='h-10 border bg-stone-300 text-gray-500'>
             <td>{formatDate(item.date)}</td>
             <td className={`text-white font-bold ${item.category == 'food' ? 'bg-orange-500' : ''}`}>{categories[item.category].title}</td>
             <td>{item.description}</td>
-            <td>$ {item.value}</td>
+            <td className={`${categories[item.category].expense === true ? 'text-red-600 font-bold' : 'text-green-600 font-bold'}`}>$ {item.value}</td>
         </tr>
     )
 }
